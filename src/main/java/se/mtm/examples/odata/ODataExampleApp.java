f9ec030fc86180d13ebb7d3984640e6d98ab6328
Jakob Eklund <jakob.eklund@mtm.se>
2018-06-27 15:02:37 +0200

    [DIST-1349] List 'People' from the reference service

--- src/main/java/se/mtm/examples/odata/ODataExampleApp.java
+++ src/main/java/se/mtm/examples/odata/ODataExampleApp.java
@@ -2,9 +2,14 @@ package se.mtm.examples.odata;
 
 
 import org.apache.olingo.client.api.ODataClient;
+import org.apache.olingo.client.api.domain.ClientEntitySet;
 import org.apache.olingo.client.api.domain.ClientServiceDocument;
 import org.apache.olingo.client.core.ODataClientFactory;
-import org.apache.olingo.commons.api.edm.*;
+import org.apache.olingo.commons.api.edm.Edm;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *  Example using the [OData Reference Service](http://www.odata.org/odata-services/)
@@ -19,6 +24,17 @@ public class ODataExampleApp {
     public static void main(String[] args){
         ODataClient client =
                 ODataClientFactory.getClient();
+
+        // List people
+        final URI peopleUri =
+                client.newURIBuilder(REFERENCE_SERVICE_URL).appendEntitySetSegment("People").build();
+        final ClientEntitySet people =
+                client.getRetrieveRequestFactory().getEntitySetRequest(peopleUri).execute().getBody();
+        final List<String> peoplesNames = people.getEntities().stream()
+                .map((person) -> person.getProperty("FirstName").getValue() + " " + person.getProperty("LastName").getValue())
+                .collect(Collectors.toList());
+        System.out.println("Peoples names: " + peoplesNames );
+
 
         // Fetch, then print, the service document (simplified service descriptor)
         final ClientServiceDocument serviceDocument =
